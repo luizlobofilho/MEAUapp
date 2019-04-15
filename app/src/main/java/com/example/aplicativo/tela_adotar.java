4c0caf86f1e53d33ee3e1ca62e3ed85d30b1d2bf
Marcos Paes <marcospaes35@gmail.com>
2019-04-15 04:17:33 -0300

    Criação do esqueleto da segunda tela "Cadastro", e linkagem com botão 'Cadastrar' da tela inicial.
    Retirada do comentário que estava impedindo a build de ser feita.

--- app/src/main/java/com/example/aplicativo/tela_adotar.java
+++ app/src/main/java/com/example/aplicativo/tela_adotar.java
@@ -3,14 +3,16 @@ package com.example.aplicativo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.Button;
 
 public class tela_adotar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_adotar);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -25,6 +27,18 @@ public class tela_adotar extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        Button btn_cadastrar_animal = (Button) findViewById(R.id.botao_cadastrar);
+
+        btn_cadastrar_animal.setOnClickListener(R.id.botao_cadastrar){
+
+        public void onClick (View v) {
+                Intent it = new  Intent (MainActivity, tela_cadastro_animal);
+                startActivity(it);
+        }
+        }
     }
 
 }
